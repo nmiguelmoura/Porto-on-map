1a235ef3a6b8589cb471b26ed2b0a4276609e1ea
nmiguelmoura <nmiguelmoura@gmail.com>
2017-01-21 15:53:32 +0000

    type: feat - Complete router functions with render_templates and jsonify

--- www/project.py
+++ www/project.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, jsonify, url_for
 from flask import session as login_session
+import prefabs.database_interaction
 
 
 app = Flask(__name__)
 
+db = prefabs.database_interaction.Database_interaction()
+
 
 @app.route('/')
 def show():
-    return 'teste'
+    return render_template('index.html')
 
 
 @app.route('/markers/JSON/')
 def return_markers():
-    return 'All markers'
+    markers = db.query_all_markers()
+    return jsonify(Marker=[m.serialize for m in markers])
 
 
 @app.route('/favourites/<int:user_id>/JSON/')
 def return_user_favourites(user_id):
-    return 'Favourites from user %s.' % user_id
+    favourites = db.query_user_favourites(user_id)
+    return jsonify(Favourite=[f.serialize for f in favourites])
 
 
 if __name__ == '__main__':
