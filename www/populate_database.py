295c68872be26b696852601305555fc8eb07c314
Nuno Machado <nmiguelmoura@gmail.com>
2017-01-29 16:35:07 +0000

    type: test - Add data to populate database on startup

--- www/populate_database.py
+++ www/populate_database.py
@@ -14,20 +14,110 @@ class Populate_database:
 
     users = [
         {
-            'name': 'me',
-            'email': 'me@email.com',
-            'picture': 'http://placekitten.com/100/100'
+            'name': 'Nuno Machado',
+            'email': 'nmiguelmoura@gmail.com',
+            'picture': 'https://scontent.xx.fbcdn.net/v/t1.0-1/c0.16.200.200/p200x200/1378145_638433192868456_594681303_n.jpg?oh=7d242c97f8f2c8a8bd4ab21ab47ba4d9&oe=59033A63'
          }
     ]
 
     markers = [
         {
-            'title': 'Place to go eat something',
-            'latitude': 41.1562453,
-            'longitude': -8.6613685,
-            'description': 'My city',
-            'user_id': 1
-        }
+            'title': u'Porto Cathedral',
+            'latitude': 41.14285504813144,
+            'longitude': -8.611221313476562,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'monument'
+        },
+        {
+            'title': u'Clérigos tower',
+            'latitude': 41.145698998819604,
+            'longitude': -8.614654541015625,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'monument'
+        },
+        {
+            'title': u'Soares dos Reis National Museum',
+            'latitude': 41.145698998819604,
+            'longitude': -8.614654541015625,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'monument'
+        },
+        {
+            'title': u'Palácio da Bolsa',
+            'latitude': 41.14138454798386,
+            'longitude': -8.615684509277344,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'monument'
+        },
+        {
+            'title': u'San Francis Church',
+            'latitude': 41.14101287789148,
+            'longitude': -8.615727424621582,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'monument'
+        },
+        {
+            'title': u'Alfândega do Porto',
+            'latitude': 41.14317823055742,
+            'longitude': -8.62152099609375,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'museum'
+        },
+        {
+            'title': u"Porto's planetarium",
+            'latitude': 41.15071600881435,
+            'longitude': -8.638461828231812,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'museum'
+        },
+        {
+            'title': u'Electric car museum',
+            'latitude': 41.14758951142637,
+            'longitude': -8.632936477661133,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'museum'
+        },
+        {
+            'title': u'Ipanema Park Hotel',
+            'latitude': 41.15444014316899,
+            'longitude': -8.649802207946777,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'hotel'
+        },
+        {
+            'title': u'Cais de Gaia Restaurants',
+            'latitude': 41.13765159183658,
+            'longitude': -8.61851692199707,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'restaurant'
+        },
+        {
+            'title': u'Cais de Gaia Coffee Shops',
+            'latitude': 41.13749806744656,
+            'longitude': -8.6172616481781,
+            'description': u'My city',
+            'user_id': 1,
+            'type': 'coffee'
+        },
+
+        {
+            'title': u'Arrábida Bridge',
+            'latitude': 41.14754103742501,
+            'longitude': -8.640317916870117,
+            'description': u'Best sunset in town',
+            'user_id': 1,
+            'type': 'other'
+        },
     ]
 
     favourites = [
@@ -54,7 +144,8 @@ class Populate_database:
                 latitude=m['latitude'],
                 longitude=m['longitude'],
                 description=m['description'],
-                user_id=m['user_id']
+                user_id=m['user_id'],
+                type=m['type']
             ))
         self.session.commit()
 
